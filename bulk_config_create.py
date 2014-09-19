fb8f489891fcc1b7fc23e96608eabd7c56114428
Akshay Rao <akshay@skytap.com>
2014-09-19 19:51:10 -0400

    Updated License
    Updated License Comment Text

--- bulk_config_create.py
+++ bulk_config_create.py
@@ -1,5 +1,32 @@
 #! /usr/bin/python
 
+'''
+Copyright 2014 Skytap Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+#-----------------------------------------------------------------------
+A python script to run a  HTTP POST command to the Skytap API
+to add a new schedule to an existing configuration, outputs json.
+This is a very simple python script to serve as an example
+for beginner to get started making API calls to Skytap.
+
+This code was tested with python 2.7.5
+
+Note: requires the requests python module which is
+open source (Apache2 licensed) and can be installed via Pip
+'''
+
 import sys
 import requests
 import json
